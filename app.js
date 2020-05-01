49fb66ed38281c3d2eabcc5a4df136b10b214a6b
Lea Rosema <terabaud@gmail.com>
2020-05-02 00:30:35 +0200

    chore: initial commit

---
+++ app.js
@@ -0,0 +1,128 @@
+const $ = document.querySelector.bind(document);
+const fontCanvas = $(".font-canvas");
+const charCanvas = $(".char-canvas");
+const fontCtx = fontCanvas.getContext("2d");
+const charCtx = charCanvas.getContext("2d");
+
+const state = {
+  numRows: 8,
+  font: null,
+  currentChar: 65,
+  pixelSize: 2,
+  zoomPixelSize: 24,
+};
+
+function loadFont(fileName) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "arraybuffer";
+    xhr.open("GET", fileName);
+    xhr.onload = () => {
+      resolve(new Uint8Array(xhr.response));
+    };
+    xhr.onerror = (err) => reject(err);
+    xhr.send();
+  });
+}
+
+function hexDump(data) {
+  return Array.prototype.concat
+    .apply([], data)
+    .map((c) => c.toString(16))
+    .join("");
+}
+
+/**
+ * Render the character table
+ * @param {*} state the state object
+ * @param {*} specificChar if specified, only a specific char is updated in the render.
+ */
+function renderFontCanvas(state, specificChar) {
+  const { pixelSize } = state;
+  for (let y = 0; y < 16; y++) {
+    for (let x = 0; x < 16; x++) {
+      const n = y * 16 + x;
+      if (typeof specificChar !== "undefined" && specificChar !== n) {
+        continue;
+      }
+      fontCtx.fillStyle = state.currentChar === n ? "#f0f" : "#444";
+      fontCtx.fillRect(
+        x * 10 * pixelSize,
+        y * (state.numRows + 2) * pixelSize,
+        10 * pixelSize,
+        (state.numRows + 2) * pixelSize
+      );
+      for (let j = 0; j < state.numRows; j++) {
+        const line = state.font[n * state.numRows + j];
+        for (let i = 0; i < 8; i++) {
+          fontCtx.fillStyle = (line & (1 << (7 - i))) > 0 ? "#aaa" : "#000";
+          fontCtx.fillRect(
+            (x * 10 + i + 1) * pixelSize,
+            (y * (state.numRows + 2) + j + 1) * pixelSize,
+            pixelSize,
+            pixelSize
+          );
+        }
+      }
+    }
+  }
+}
+
+function renderCharacter(state) {
+  const pixelSize = state.zoomPixelSize;
+  const n = state.currentChar;
+  for (let j = 0; j < state.numRows; j++) {
+    const line = state.font[n * state.numRows + j];
+    for (let i = 0; i < 8; i++) {
+      charCtx.fillStyle = (line & (1 << (7 - i))) > 0 ? "#aaa" : "#000";
+      charCtx.fillRect(
+        1 + i * (pixelSize + 1),
+        1 + j * (pixelSize + 1),
+        pixelSize,
+        pixelSize
+      );
+    }
+  }
+}
+
+function setup() {
+  loadFont("8X16.BIN").then((font) => {
+    state.numRows = (font.length / 256) | 0;
+    state.font = font;
+    fontCanvas.width = 10 * state.pixelSize * 16;
+    fontCanvas.height = (state.numRows + 2) * state.pixelSize * 16;
+    charCanvas.width = 1 + 9 * state.zoomPixelSize;
+    charCanvas.height = 1 + (state.numRows + 1) * state.zoomPixelSize;
+    renderFontCanvas(state);
+    renderCharacter(state);
+  });
+
+  fontCanvas.addEventListener("click", (e) => {
+    if (!state.font) {
+      return;
+    }
+    const { pixelSize, numRows } = state;
+    const x = e.clientX - fontCanvas.offsetLeft + window.scrollX;
+    const y = e.clientY - fontCanvas.offsetTop + window.scrollY;
+    const xIndex = (x / (10 * pixelSize)) | 0;
+    const yIndex = (y / ((numRows + 2) * pixelSize)) | 0;
+    state.currentChar = yIndex * 16 + xIndex;
+    renderFontCanvas(state);
+    renderCharacter(state);
+  });
+
+  charCanvas.addEventListener("click", (e) => {
+    const pixelSize = state.zoomPixelSize;
+    const x = e.clientX - charCanvas.offsetLeft + window.scrollX;
+    const y = e.clientY - charCanvas.offsetTop + window.scrollY;
+    const xIndex = ((x - 1) / (pixelSize + 1)) | 0;
+    const yIndex = ((y - 1) / (pixelSize + 1)) | 0;
+    console.log(xIndex, yIndex);
+    const line = state.currentChar * state.numRows + yIndex;
+    state.font[line] ^= 1 << (7 - xIndex);
+    renderCharacter(state);
+    renderFontCanvas(state, state.currentChar);
+  });
+}
+
+setup();

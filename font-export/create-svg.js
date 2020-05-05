c84e27cb1673173e0d192d49a600da59fccecf08
Lea Rosema <terabaud@gmail.com>
2020-05-06 00:01:00 +0200

    chore: try things out

--- font-export/create-svg.js
+++ font-export/create-svg.js
@@ -43,8 +43,7 @@ function createSVG(font) {
 function createGlyphSVG(font) {
   const numRows = (font.length / 256) | 0;
   const viewBox = [0, 0, 8 * 16, numRows * 16].join(" ");
-  const symbolViewBox = [0, 0, 8, numRows];
-  const symbols = [];
+  const glyphs = [];
   const content = [];
   for (let n = 0; n < 256; n++) {
     let path = "";
@@ -52,36 +51,32 @@ function createGlyphSVG(font) {
       const line = font[n * numRows + y];
       for (let x = 0; x < 8; x++) {
         if ((line & (1 << (7 - x))) > 0) {
-          path += `M${x} ${y}H${x + 1}V${y + 1}H${x}Z`;
+          path += `M${x} ${numRows - 1 - y}H${x + 1}V${numRows - y}H${x}Z`;
         }
       }
     }
-    symbols.push(
-      `<symbol id="chr${n}" viewBox="${symbolViewBox}" fill="#000"><path d="${path}" /></symbol>`
+    glyphs.push(
+      `<glyph unicode="&#x${
+        (n < 16 ? "0" : "") + n.toString(16)
+      };" d="${path}" />`
     );
     const xPos = (n % 16) * 8;
     const yPos = ((n / 16) | 0) * numRows;
     content.push(
-      `<use xlink:href="#chr${n}" x="${xPos}" y="${yPos}" width="8" height="${numRows}"/>`
+      `<text x="${xPos}" y="${yPos}" font-size="${numRows}" />&#x${
+        (n < 16 ? "0" : "") + n.toString(16)
+      };</text>`
     );
   }
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="${viewBox}">
-      ${symbols}
-      ${content}
-    </svg>`;
-
-  const glyphSvg = `
-  <svg version="1.1" viewBox="0 0 160 70" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    
+  const svg = `<svg version="1.1" viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
         <font horiz-adv-x="8" xml:id="Mu">
-            <metadata>8Bit Mono</metadata>
-            <font-face font-family="leatest" units-per-em="16"/>
-            <missing-glyph d="M0 0 H8 V1 H0Z"/>
-            <glyph unicode="X" d="M0 15 H1 V16 H0ZM7 0 H8 V1 H7ZM0 0 H1 V1 H0Z M1 1 H2 V2 H1ZM2 2 H3 V3 H2ZM3 3 H4 V4 H3Z M4 4 H5 V5 H4 M5 5 H6 V6 H5Z M6 6 H7 V7 H6ZM7 7 H8 V8 H7Z"/>
+            <metadata>EightBitMono</metadata>
+            <font-face font-family="EightBitMono" units-per-em="${numRows}"/>
+            ${glyphs.join("")}
         </font>
     </defs>
-    <text x="0" y="70" font-family="leatest" font-size="16" fill="#933">XXyX</text>
+    ${content.join("")}
   </svg>`;
   return svg;
 }

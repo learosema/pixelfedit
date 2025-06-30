d017d515d82748d9bb4e5acdede79775d5328ca8
Lea Rosema <lea@lea.lgbt>
2025-06-30 23:48:59 +0200

    chore: add some basic font generation

--- src/js/generative/gen-from-font.js
+++ src/js/generative/gen-from-font.js
@@ -13,19 +13,20 @@ export function generateFromFont(state, font, from, to, width = 8, height = 16) 
     ctx.fillStyle = "#fff";
     const character = String.fromCodePoint(CP437_TABLE.codePointAt(i))
     const measure = ctx.measureText(character);
-    const charWidth =  measure.width;
-    ctx.fillText(character, 0, baseline);
+    const charWidth =  measure.width + (sourceSize >> 3);
+    const monoWidth = Math.max(8, charWidth)
+    ctx.fillText(character, sourceSize - charWidth / 2, baseline);
     const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     for (let y = 0; y < height; y++) {
       let row = 0;
       for (let x = 0; x < width; x++) {
-        const sx = Math.round(Math.max(8, charWidth) * x / width);
+        const sx = Math.round(sourceSize - monoWidth / 2 + monoWidth * x / width);
         const sy = Math.round(canvas.height * y / height);
         const b = imgData.data[(sy*canvas.width + sx)*4];
         const b1 = imgData.data[(sy*canvas.width + sx + 1)*4];
         const b2 = imgData.data[((sy+1)*canvas.width + sx)*4];
         const b3 = imgData.data[((sy+1)*canvas.width + sx + 1)*4];
-        if (b > 200 || (b+b1+b2+b3) > 100) {
+        if (b > 127 || (b+b1+b2+b3) > 255) {
           row |= (1<<(7-x))
         }
       }
